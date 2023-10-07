67d662d6258cc7496ac2cbc2d057e737a0b9ba8b
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-07 19:02:47 +0530

    Added some styles to card

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -7,7 +7,7 @@ const PlayerCard = ({ playerInfo = {} }) => {
   const age = getYearsBetweenDates(dob);
 
   return (
-    <div className="p-2 m-2 border border-black w-52 h-64">
+    <div className="p-2 m-2 border border-black w-52 h-64 hover:bg-neutral-300 cursor-pointer shadow-lg rounded-lg">
       <h2>{name}</h2>
       <h2>{type}</h2>
       <h2>{points}</h2>
