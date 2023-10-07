1ddfcb46a0ff949bbd83f2dab68641f957166576
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-07 15:12:42 +0530

    Added players test data and helper method to get players

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-  return <div className="p-2 m-2">Header</div>;
+  return <div className="p-2 m-2 bg-gray-300">Cricketer's app</div>;
 };
 
 export default Header;
