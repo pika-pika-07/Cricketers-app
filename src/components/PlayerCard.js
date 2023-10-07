58d462ac3cd54f1a976842ef2ed009add863c5d1
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-07 18:55:50 +0530

    Removed commented code

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -5,14 +5,6 @@ const PlayerCard = ({ playerInfo = {} }) => {
   const { name, type, points, rank, dob, description } = playerInfo;
 
   const age = getYearsBetweenDates(dob);
-  debugger;
-  //   const date = new Date(dob);
-  //   console.log(date);
-  //   const currentDate = new Date();
-  //   console.log(currentDate);
-  //   const timeDiff = currentDate.getTime() - date.getTime();
-  //   const days = timeDiff / (1000 * 3600 * 24);
-  //   const years = Math.abs(Math.round(days / 365));
 
   return (
     <div className="p-2 m-2 border border-black w-52 h-64">
