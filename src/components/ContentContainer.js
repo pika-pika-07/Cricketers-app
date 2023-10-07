30ff2b6576fe19e4fb8b0d85b440efbf4364b16a
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-07 16:33:29 +0530

    Added a hook to fetch players from the helper function

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useFetchPlayers from "../hooks/useFetchPlayers";
 import PlayerCard from "./PlayerCard";
 
 const ContentContainer = () => {
+  const players = useFetchPlayers();
   return (
     <div className="flex p-6 m-6  flex-wrap">
       <PlayerCard />
---
+++ src/hooks/useFetchPlayers.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import getPlayers from "../utils/helper";
+
+const useFetchPlayers = () => {
+  const [players, setPlayers] = useState(null);
+  const fetchPlayers = async () => {
+    const data = await getPlayers();
+
+    setPlayers(data);
+  };
+
+  useEffect(() => {
+    fetchPlayers();
+  }, []);
+  return players;
+};
+
+export default useFetchPlayers;
