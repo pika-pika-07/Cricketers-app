84e76c10b51c789fdc8c9cef1f21494b5f06bb13
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-07 14:10:35 +0530

    Added a basic scafold for our react app, added Header and Body Components,rendered it insider root of react

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+import Body from "./src/components/Body";
+import Header from "./src/components/Header";
+
+const heading = <h1> Hello jsx </h1>;
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+};
+const root = ReactDOM.createRoot(document.querySelector("#root")); // Creating a Root for React
+
+root.render(<AppLayout />); // Rendering the APPlayout inside root
