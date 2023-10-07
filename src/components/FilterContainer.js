176ecc0eac10ba166ec3bee2b99a01e92dee2c78
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-07 20:01:17 +0530

    Added dropdown for sort filter

--- src/components/FilterContainer.js
+++ src/components/FilterContainer.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const FilterContainer = () => {
-  return <div className="mx-5 p-5 bg-gray-300">FilterContainer</div>;
+  return (
+    <div className="mx-5 p-5 bg-gray-300">
+      <div className="">
+        <select value={"name"} onChange={() => {}}>
+          <option name="name"> Name </option>
+          <option name="age"> Age </option>
+          <option name="rank"> Rank </option>
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default FilterContainer;
