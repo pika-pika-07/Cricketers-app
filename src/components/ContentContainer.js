eaa7957c2b3c16352aa090ac178ed32070710afb
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-09 14:18:49 +0530

    Added pagination

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -11,10 +11,19 @@ import {
 const ContentContainer = () => {
   const [players, setPlayers] = useState([]);
   const [allPlayers, setAllPlayers] = useState([]);
+  const [playersToShow, setPlayersToShow] = useState([10]);
   const [searchParams, setSearchParams] = useSearchParams();
   const sortBy = searchParams.get("sort");
   const filterBy = searchParams.get("filter");
   const searchBy = searchParams.get("search");
+
+  useEffect(() => {
+    fetchCricketers();
+  }, []);
+
+  useEffect(() => {
+    filterPlayers(players);
+  }, [sortBy, filterBy, searchBy]);
 
   const filterPlayers = (players) => {
     if (!sortBy && !filterBy && !searchBy && allPlayers.length > 0) {
@@ -27,7 +36,6 @@ const ContentContainer = () => {
       setPlayers(sortedPlayers);
     }
     if (filterBy) {
-      // const newList = [...players];
       const filteredPlayers = filterPlayersByType(allPlayers, filterBy);
       setPlayers(filteredPlayers);
     }
@@ -42,26 +50,30 @@ const ContentContainer = () => {
     setPlayers(data);
     filterPlayers(data);
   };
-  useEffect(() => {
-    fetchCricketers();
-  }, []);
 
-  useEffect(() => {
-    filterPlayers(players);
-  }, [sortBy, filterBy, searchBy]);
+  const handleShowMore = () => {
+    setPlayersToShow(playersToShow < players.length ? players.length : 10);
+  };
 
   if (players.length === 0) return <div>Loading</div>;
   return (
-    <div className="flex p-6 m-6  flex-wrap">
-      {players.map((player) => {
-        return (
-          <Link key={player.id} to={`player?id=${player.id}`}>
-            {" "}
-            <PlayerCard playerInfo={player} />
-          </Link>
-        );
-      })}
-    </div>
+    <>
+      <div className="flex p-6 m-6  flex-wrap">
+        {players.slice(0, playersToShow).map((player) => {
+          return (
+            <Link key={player.id} to={`player?id=${player.id}`}>
+              {" "}
+              <PlayerCard playerInfo={player} />
+            </Link>
+          );
+        })}
+      </div>
+      <div className="border border-black w-[7%] m-3 p-3 mx-14">
+        <button onClick={handleShowMore}>
+          {playersToShow === players.length ? "Show Less" : "Show More"}
+        </button>
+      </div>
+    </>
   );
 };
 
