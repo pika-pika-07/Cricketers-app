dc5a7f3b41ec92b1853995c238e7e7763aaf98b9
Pika-Pika-07 <parthbhatnagar40@gmail.com>
2023-10-09 15:58:34 +0530

    Added a logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,18 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <Link to="/">
-      <div className="p-5 m-5 bg-gray-300">Cricketer's app</div>
-    </Link>
+    <div className="p-5 m-5 bg-gray-300  flex ">
+      <Link to="/">
+        <img
+          className="w-40"
+          src="https://t3.ftcdn.net/jpg/02/02/27/68/240_F_202276862_Bl20MC12CxPZAr781Sctf37otHIwSvBU.jpg"
+        />
+      </Link>
+      <div className="p-10 font-bold text-center align-middle">
+        {" "}
+        Cricketer's app
+      </div>
+    </div>
   );
 };
 
